d6989a688fb36c1dc3106442d19609537225641c
umairayub79 <umairayub79@gmail.com>
2021-04-06 17:40:05 +0500

    first commit

---
+++ scripts/chip8.js
@@ -0,0 +1,38 @@
+import Renderer from './renderer.js'
+import Keyboard from './keyboard.js'
+import Speaker from './speaker.js'
+import CPU from './cpu.js'
+
+const renderer = new Renderer(10)
+const keyboard = new Keyboard()
+const speaker = new Speaker()
+const cpu = new CPU(renderer,keyboard,speaker)
+
+let loop
+
+let fps = 60, fpsInterval, startTime, now, then, elapsed
+
+function init() {
+    fpsInterval = 1000 / fps
+    then = Date.now()
+    startTime = then
+
+    renderer.render()
+
+    cpu.loadSpritesIntoMemory()
+    cpu.loadRom('PONG')
+    loop = requestAnimationFrame(step)
+}
+
+function step() {
+    now = Date.now()
+    elapsed = now - then
+
+    if (elapsed > fpsInterval) {
+        cpu.cycle()
+    }
+
+    loop = requestAnimationFrame(step)
+}
+
+init()
